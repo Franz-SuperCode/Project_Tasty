c525c1478d10eab247533439b978d0c27d787067
Franz-SuperCode <franzsupercode@gmail.com>
2022-12-12 17:13:49 +0100

    wiedergeholt

--- src/components/randomCard/RandomCard.js
+++ src/components/randomCard/RandomCard.js
@@ -1,11 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import "./RandomCard.css"
 
 
 function RandomCard() {
+    const [randomRecipe, setRandomRecipe] = useState();
+    useEffect(() => {
+        fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+            .then(response => response.json())
+            .then(randomAPI => {
+                setRandomRecipe(randomAPI.meals[0]);
+                console.log(randomAPI.meals[0]);
+            });
+    }, []);
+
+    // Bild mit Text in einen Container der einen Hintergrund hat
     return (
         <div className="RandomCard">
-            RandomCard
+            <h2>Meal of the Day</h2>
+            <img alt={randomRecipe?.strMeal} src={`${randomRecipe?.strMealThumb}`} />
         </div>
     );
+
 }
 
 export default RandomCard;
