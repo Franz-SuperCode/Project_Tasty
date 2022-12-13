5aa88f3d30122df92dd286b070c508896f0397db
Franz-SuperCode <franzsupercode@gmail.com>
2022-12-13 10:41:54 +0100

    all area buttons

--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -1,9 +1,33 @@
-
+import "./Buttons.css"
+import { useEffect, useState } from "react";
 
 function Buttons() {
+
+    const [areas, setAreas] = useState();
+    useEffect(() => {
+        fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+            .then(response => response.json())
+            .then(areasAPI => {
+                setAreas(areasAPI.meals);
+
+            });
+    }, []);
+
+
+
+
     return (
         <div className="Buttons">
-            Buttons
+
+            {areas?.map((object, index) => {
+                return (
+                    //Alle Area- Bezeichnungen geholt
+                    <button key={index}>{object.strArea}</button>
+                )
+
+            })}
+
+
         </div>
     );
 }
