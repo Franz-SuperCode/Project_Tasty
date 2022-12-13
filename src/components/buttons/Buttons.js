3fe9d6dc48cd68cad4a238848664a292ce9e5114
Franz-SuperCode <franzsupercode@gmail.com>
2022-12-13 15:02:29 +0100

    area & categorie

--- src/components/buttons/Buttons.js
+++ src/components/buttons/Buttons.js
@@ -1,35 +1,13 @@
 import "./Buttons.css"
 import { useEffect, useState } from "react";
 
-function Buttons() {
 
-    const [areas, setAreas] = useState();
-    useEffect(() => {
-        fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
-            .then(response => response.json())
-            .then(areasAPI => {
-                setAreas(areasAPI.meals);
-
-            });
-    }, []);
-
-
-
-
-    return (
-        <div className="Buttons">
-
-            {areas?.map((object, index) => {
-                return (
-                    //Alle Area- Bezeichnungen geholt
-                    <button key={index}>{object.strArea}</button>
-                )
-
-            })}
-
-
-        </div>
-    );
+function Buttons(props) {
+    <div>
+        <button>{props[0]}</button>
+        <button>{props[1]}</button>
+        <button>{props[2]}</button>
+    </div>
 }
 
 export default Buttons;
