0fe93f365d160ddc646a8f69728eb7263a1f1db6
Franz-SuperCode <franzsupercode@gmail.com>
2022-12-14 20:59:19 +0100

    add seeAll Home function

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -13,7 +13,42 @@ import profil from "../../assets/img/profilWhite.svg"
 
 
 
+
 function Home() {
+
+    //Fetch Areas
+    const [areas, setAreas] = useState();
+    useEffect(() => {
+        fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+            .then(response => response.json())
+            .then(areasAPI => {
+                setAreas(areasAPI.meals);
+                console.log(areasAPI.meals)
+            });
+    }, []);
+
+    //Fetch Categories
+    const [categories, setCategories] = useState();
+    useEffect(() => {
+        fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
+            .then(response => response.json())
+            .then(categoriesAPI => {
+                setCategories(categoriesAPI.meals);
+                console.log(categoriesAPI.meals);
+            });
+    }, []);
+
+
+    const [seeArea, setSeeArea] = useState("detailsShown");
+    const [seeCategory, setSeeCategory] = useState("detailsShown");
+    // const [catOrAr, setCatOrAr] = useState("strArea");
+    //Alle Buttons anzeigen / verstecken
+    function showAllArea() {
+        seeArea === "detailsShown" ? setSeeArea("detailsHidden") : setSeeArea("detailsShown");
+    }
+    function showAllCategory() {
+        seeCategory === "detailsShown" ? setSeeCategory("detailsHidden") : setSeeCategory("detailsShown");
+    }
 
     return (
         <div className="Home">
@@ -21,17 +56,18 @@ function Home() {
             <RandomCard />
             <section className="homeAreaWrapper">
                 <h2>Areas</h2>
-                <SeeAll
+                {/* <SeeAll
                     allLink={"/search/areas"}
-                />
+                /> */}
                 {/* ========== */}
-                {/* <div className="buttonCloud">
-                   
-                    {areasList?.map((singleArea, index) => {
-                        return <Buttons key={index} buttonName={singleArea.strArea
-                        } classBtn={index >= 2 ? see : " "} buttonLink={`/search/${singleArea.strArea}`} />
-                    })}
-                </div> */}
+                <button onClick={showAllArea} className="seeAllButton">seeAll</button>
+                {areas?.map((object, index) => {
+                    console.log(object.strArea)
+                    return (
+                        <button className={seeArea} key={index}>{object.strArea}</button>
+                    )
+
+                })}
                 {/* ============ */}
             </section>
             <section className="areaButtonsWrapper">
@@ -55,6 +91,18 @@ function Home() {
                 <SeeAll
                     allLink={"/search/category"}
                 />
+                {/* ========== */}
+                <button onClick={showAllCategory} className="seeAllButton">seeAll</button>
+                {categories?.map((object, index) => {
+                    console.log(object.strCategory);
+                    console.log(categories);
+
+                    return (
+                        <button className={seeCategory} key={index}>{object.strCategory}</button>
+                    )
+
+                })}
+                {/* ============ */}
             </section>
 
             <div className="catButtonWrapper">
