84a651ac43c152b19e289e8792084ec805db6d44
sofia28304 <sofia.vogelhaupt@yahoo.de>
2022-12-15 10:08:24 +0100

    styling home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -16,44 +16,43 @@ import profil from "../../assets/img/profilWhite.svg"
 function Home() {
 
     return (
-        <div className="Home">
-            <Searchbar />
-            <RandomCard />
-            <section className="homeAreaWrapper">
-                <h2>Areas</h2>
-                <SeeAll
-                    allLink={"/search/areas"}
-                />
-            </section>
-            <section className="areaButtonsWrapper">
-                <Buttons
-                    buttonName={"American"}
-                    buttonLink={"/search/American"}
-                />
-                <Buttons
-                    buttonName={"British"}
-                    buttonLink={"/search/British"}
-                />
-                <Buttons
-                    buttonName={"Dutch"}
-                    buttonLink={"/search/Dutch"}
-                />
-            </section>
+        <div>
+            <div className="Home">
+                <Searchbar />
+                <RandomCard />
+                <section className="homeAreaWrapper">
+                    <h2>Areas</h2>
+                    <SeeAll
+                        allLink={"/search/areas"}
+                    />
+                </section>
+                <section className="areaButtonsWrapper">
+                    <Buttons
+                        buttonName={"American"}
+                        buttonLink={"/search/American"}
+                    />
+                    <Buttons
+                        buttonName={"British"}
+                        buttonLink={"/search/British"}
+                    />
+                    <Buttons
+                        buttonName={"Dutch"}
+                        buttonLink={"/search/Dutch"}
+                    />
+                </section>
+                <section className="homeCatWrapper">
+                    <h2>Categories</h2>
+                    <SeeAll
+                        allLink={"/search/category"}
+                    />
+                </section>
 
-
-            <section className="homeCatWrapper">
-                <h2>Categories</h2>
-                <SeeAll
-                    allLink={"/search/category"}
-                />
-            </section>
-
-            <div className="catButtonWrapper">
-                <CategoryButtons url={"https://www.themealdb.com/images/category/beef.png"} cat={"Beef"} link={"/search/Beef"} />
-                <CategoryButtons url={"https://www.themealdb.com/images/category/chicken.png"} cat={"Chicken"} link={"/search/Chicken"} />
-                <CategoryButtons url={"https://www.themealdb.com/images/category/dessert.png"} cat={"Dessert"} link={"/search/Dessert"} />
+                <div className="catButtonWrapper">
+                    <CategoryButtons url={"https://www.themealdb.com/images/category/beef.png"} cat={"Beef"} link={"/search/Beef"} />
+                    <CategoryButtons url={"https://www.themealdb.com/images/category/chicken.png"} cat={"Chicken"} link={"/search/Chicken"} />
+                    <CategoryButtons url={"https://www.themealdb.com/images/category/dessert.png"} cat={"Dessert"} link={"/search/Dessert"} />
+                </div>
             </div>
-          
             <Navbar
                 homeBlau={homeBlau}
                 lupeWhite={lupeWhite}
