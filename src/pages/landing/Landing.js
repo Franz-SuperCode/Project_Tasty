82963ea61eedff6ad0bd52a6fae1ed95c8a6b237
Franz-SuperCode <franzsupercode@gmail.com>
2022-12-16 10:41:36 +0100

    changes

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -10,7 +10,7 @@ function Landing() {
             <section className='whiteSection'>
                 <h1 className='lanTitel'>All the recipes you need</h1>
                 <h2 className='lanSub'>5000+ healthy recipes made by people, <br />for your healthy life</h2>
-                <Link to="/"> <button className="getStarted">Get Started</button></Link>
+                <Link to="/home"> <button className="getStarted">Get Started</button></Link>
             </section>
 
         </div>
