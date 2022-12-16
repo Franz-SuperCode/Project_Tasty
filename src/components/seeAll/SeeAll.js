a533da350de4309cfb73e18af411617c69d188af
Franz-SuperCode <franzsupercode@gmail.com>
2022-12-16 12:55:04 +0100

    kleine aenderung

--- src/components/seeAll/SeeAll.js
+++ src/components/seeAll/SeeAll.js
@@ -10,7 +10,7 @@ function SeeAll(props) {
 
     return (
         <div className="seeAll">
-            <Link to={props.allLink} onClick={props.functionBtn} className="seeAllButton">{props.name ? props.name : "SeeAll"}</Link>
+            <Link to={props.allLink} onClick={props.functionBtn} className="seeAllButton">{props.name ? props.name : "See All"}</Link>
 
         </div>
     );
